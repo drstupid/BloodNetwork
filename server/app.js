896874f77ed3151bea4ac582258aadcd03a1d21f
Norbert Nagy <norbert.nagy@3pillarglobal.com>
2016-11-18 11:57:16 +0200

    Handle and display registration form with phone number and validation code; Added loki.json to gitignore; Added "patern" module to packages;

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
  var express = require("express")
+ var bodyParser = require("body-parser");
  var app = express()
  var path = require("path")
+ var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+ var registrationController = require("./registrationController.js")
+ var db = require("./databaseController.js")
+ registrationController.initialize(db)
 
  app.listen(8080, function(){
    console.log("Server started...")
@@ -9,3 +15,41 @@
  app.get("/", function(request, response) {
    response.sendFile("index.html", {root: path.join(__dirname, "../src")})
  })
+
+// Dispaly registration form
+ app.get("/registrationForm", function(request, response) {
+   response.sendFile("registrationForm.html", {root: path.join(__dirname, "../src")})
+ })
+
+// Process registration and display phone nr validation
+app.post("/registrationAction", urlencodedParser, function(request, response) {
+  if (!request.body) return response.sendStatus(400)
+
+  var phoneNumber = request.body.phoneNumber
+  console.log("post from registrationAction: " + phoneNumber)
+  registrationController.registerPhoneNumber(phoneNumber, function(err, message) {
+    if(!err) {
+      response.sendFile("phoneNumberValidation.html", {root: path.join(__dirname, "../src")})
+    } else {
+      //TODO: replace
+      console.log("phone number registration failed")
+      response.status(500).send({erorr: "phone number registration failed"})
+    }
+  })
+})
+
+// Process phone number validation with validation code
+app.post("/phoneValidationAction", urlencodedParser, function(request, response) {
+  if (!request.body) return response.sendStatus(400)
+
+  var validationCode = request.body.validationCode;
+  console.log("post from phoneValidationAction: " + validationCode)
+  var isValid = registrationController.validatePhoneNumber("+40742128415", validationCode)
+  if (isValid) {
+    console.log("validation code is valid")
+    response.redirect("/")
+  } else {
+    console.log("validation code not valid")
+    response.status(500).send({erorr: "validation code not valid"})
+  }
+})
