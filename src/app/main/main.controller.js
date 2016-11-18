6f13690ee36036a439756994068eb15200372213
Razvan Vlasin <razvan.vlasin@office365.3pillarglobal.com>
2016-11-18 12:25:26 +0200

    Signed-off-by: Razvan Vlasin <razvan.vlasin@office365.3pillarglobal.com>

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -20,10 +20,40 @@
       interval: 2000
     })
 
+    $.getJSON("https://bloodnetwork.apispark.net/v1/locationses/?media=json", function(result){
+           $.each(result, function(i, field){
+               $("#location-centers").prepend('<li><a href="#">' + field.city + '</a></li>');
+           });
+       });
+
     $('#myModal').on('shown.bs.modal', function () {
       $('#myInput').focus()
     })
 
+$('#myModalSend').on('click', function (e) {
+    $('#myModal').modal('hide');
+    $('#myModal2').modal('show');
+    $.ajax({
+                   type: "POST",
+                   url: "http://localhost:8080/registrationAction",
+                   data: "number=+407492755545",
+                   success: function (data)
+                   {
+                      $('#myModal2').modal('show');
+                   }
+               });
+
+
+})
+
+$('#myModalCheck').on('click', function (e) {
+  if ($("#myModalCheck").is(':checked')){
+      $('#myModalSend').prop('disabled', false);
+      }
+      else{
+        $('#myModalSend').prop('disabled', true);
+      }
+})
     function activate() {
       $timeout(function() {
         vm.classAnimation = 'rubberBand';
